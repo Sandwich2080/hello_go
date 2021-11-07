810956e26dd03706682afab115fea1dffdb0d0b0
Sandwich2080 <androidsandwich@163.com>
2021-11-07 13:24:49 +0800

    first commit

---
+++ src/hello_go.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name = "Mr Jackson"
+	passcode := "123"
+	var age int = 23
+	fmt.Println("name: "+name+",passcode:"+passcode+",age:", age)
+}
+
+// dlv-dap installing guide
+// https://github.com/golang/vscode-go/blob/master/docs/debugging.md#updating-dlv-dap
