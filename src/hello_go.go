f596d64ab35ca4b2a25b3ca6d10ece3930fb03fc
androidsandwich <androidsandwich@163.com>
2021-11-07 15:44:04 +0800

    example for struct

--- src/hello_go.go
+++ src/hello_go.go
@@ -2,11 +2,21 @@ package main
 
 import "fmt"
 
+type Book struct {
+	id      int
+	title   string
+	author  string
+	subject string
+}
+
 func main() {
 	var name = "Mr Jackson"
 	passcode := "123"
 	var age int = 23
 	fmt.Println("name: "+name+",passcode:"+passcode+",age:", age)
+
+	var book1 = Book{id: 1, title: "Harry Portter", author: "Unknown", subject: "unknown"}
+	fmt.Println(book1)
 }
 
 // dlv-dap installing guide
