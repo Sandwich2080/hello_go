d91003a67a434a6af7becabd0003c45b135a002c
androidsandwich <androidsandwich@163.com>
2021-11-20 21:54:28 +0800

    'create a module' and 'how to invoke' example

--- src/hello_go.go
+++ src/hello_go.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-        "fmt"
-        "log"
-    
-        "example.com/greetings"
+	"fmt"
+	"log"
+
+	"example.com/greetings"
 )
 
 type Book struct {
@@ -15,7 +15,7 @@ type Book struct {
 }
 
 func main() {
-       
+
 	var name = "Mr Jackson"
 	passcode := "123"
 	var age int = 23
@@ -24,27 +24,30 @@ func main() {
 	var book1 = Book{id: 1, title: "Harry Portter", author: "Unknown", subject: "unknown"}
 	fmt.Println(book1)
 
-        // Get a greeting message and print it.
-        // message := greetings.Hello("Programming lovers")
-        // fmt.Println(message)
+	// Get a greeting message and print it.
+	// message := greetings.Hello("Programming lovers")
+	// fmt.Println(message)
 
-        // Set properties of the predefined Logger, including
-        // the log entry prefix and a flag to disable printing
-        // the time, source file, and line number.
-        log.SetPrefix("greetings: ")
-        log.SetFlags(0)
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
 
-        // Request a greeting message.
-        message, err := greetings.Hello("")
-        // If an error was returned, print it to the console and
-        // exit the program.
-        if err != nil {
-            log.Fatal(err)
-        }
+	// A slice of names.
+	names := []string{"Gladys", "Samantha", "Darrin"}
 
-        // If no error was returned, print the returned message
-        // to the console.
-        fmt.Println(message)
+	// Request greeting messages for the names.
+	messages, err := greetings.Hellos(names)
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message
+	// to the console.
+	fmt.Println(messages)
 }
 
 // dlv-dap installing guide
@@ -52,7 +55,3 @@ func main() {
 
 // Guide for new Go programmer
 // https://golang.org/doc/tutorial/random-greeting
-
-
-
- 
